0382fdfc2aa5e3e6f01c9d9ee93e198b515ad76a
BM <bikramcode@gmail.com>
2018-02-02 23:50:15 -0800

    Selection Sort

--- JAVA/SelectionSort.java
+++ JAVA/SelectionSort.java
@@ -1,9 +1,12 @@
 //Selection Sort
+// https://github.com/brlrb
 
 // STEPS:
 // 1: Find the smallest unsorted element in the list and add it to the end of the sorted list
 // 2: Repeat until no unsorted emelents remain
 
-// https://github.com/brlrb
-
-
+public class SelectionSort {
+    public static void main(String args[]){
+        System.out.println("Selection Sort");
+    }
+}
