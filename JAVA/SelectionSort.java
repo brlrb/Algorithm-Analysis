bcae8c34e38aa7ded0d559db82848836cabf53cf
BM <bikramcode@gmail.com>
2018-02-02 23:11:38 -0800

    What is Selection Sort

---
+++ JAVA/SelectionSort.java
@@ -0,0 +1,9 @@
+//Selection Sort
+
+// STEPS:
+// 1: Find the smallest unsorted element in the list and add it to the end of the sorted list
+// 2: Repeat until no unsorted emelents remain
+
+// https://github.com/brlrb
+
+
